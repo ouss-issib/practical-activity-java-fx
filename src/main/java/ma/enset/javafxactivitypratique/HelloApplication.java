2fce4994e863370eb6ad1a0aac23a37340a04d52
ouss issib <oussamaissib@gmail.com>
2025-01-18 22:08:16 +0100

    feat: Implement code for  Product Class

--- src/main/java/ma/enset/javafxactivitypratique/HelloApplication.java
+++ src/main/java/ma/enset/javafxactivitypratique/HelloApplication.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 
 
 //Créer une application JavaFX qui permet de gérer les produits. L'application se compose des éléments suivants:
-//1- Une classe Product avec deux attributs name et price, des constructeurs, des getters, des setters et la méthode toString.
 //2- Créer la vue product-view qui affiche un formulaire et une listeview pour afficher les produits.
 //3- Créer le controlleur ProuctController qui réponds aux actions de l'utilisateur.
 //4- Créer une feuille de style css à appliquer sur l'interface graphique.
---
+++ src/main/java/ma/enset/javafxactivitypratique/Product.java
@@ -0,0 +1,38 @@
+package ma.enset.javafxactivitypratique;
+
+public class Product {
+    private String name;
+    private String price;
+
+    public Product() {
+    }
+
+    public Product(String name, String price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product { " +
+                "name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+}
