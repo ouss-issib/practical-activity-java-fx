c4b05f83d60da6aedf449d0fc9becce67dcf505e
ouss issib <oussamaissib@gmail.com>
2025-01-19 00:36:04 +0100

    fix: delete HelloApplication and hello-view

--- src/main/java/ma/enset/javafxactivitypratique/ProductApplication.java
+++ src/main/java/ma/enset/javafxactivitypratique/ProductApplication.java
@@ -2,22 +2,22 @@ package ma.enset.javafxactivitypratique;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-
-public class HelloApplication extends Application {
+public class ProductApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("product-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 450, 500);
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root,450,500);
         scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
         stage.setTitle("Product Management Application");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
