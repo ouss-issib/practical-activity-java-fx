9c5eb1d456fef09bfbca713aa45cd3cce0619ac2
ouss issib <oussamaissib@gmail.com>
2025-01-19 00:31:27 +0100

    style: add stylesheet file to our application

--- src/main/java/ma/enset/javafxactivitypratique/HelloApplication.java
+++ src/main/java/ma/enset/javafxactivitypratique/HelloApplication.java
@@ -8,18 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 
-//Créer une application JavaFX qui permet de gérer les produits. L'application se compose des éléments suivants:
-//2- Créer la vue product-view qui affiche un formulaire et une listeview pour afficher les produits.
-//3- Créer le controlleur ProuctController qui réponds aux actions de l'utilisateur.
-//4- Créer une feuille de style css à appliquer sur l'interface graphique.
-//5- Créer une classe application avec la méthode start pour démarer l'application.
-
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("product-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 450, 500);
+        scene.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
         stage.setTitle("Product Management Application");
         stage.setScene(scene);
         stage.show();
